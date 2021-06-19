feaa6221a760b48dbfe07725121c8bc1f3184ffb
Dhruv2110 <dhruvsharma.ynr@gmail.com>
2021-06-19 11:25:55 +0530

    Enquiry Button added

--- Components/ImageCard.js
+++ Components/ImageCard.js
@@ -1,18 +1,35 @@
 import React from 'react';
 
-import { View, Text, StyleSheet, Image } from 'react-native'
+import { View, Text, StyleSheet, Image, TouchableOpacity, Linking } from 'react-native'
 
 const ImageCard = ( {src,model} ) => {
     //alert(src)
     //let path = require('../assets/images/4.jpg')
+    
+    const whatsapp = () => {
+        let msg = 'Hi, i am interested in this model : ';
+        if (msg) {
+            let url = "whatsapp://send?text=" + msg + model + "&phone=918818000994";
+            Linking.openURL(url)
+        } else {
+            alert("Please enter message to send");
+        }
+    }
+
     return (
                 <View style={styles.imageCard}>
+            <Text style={styles.productModel}>{model}</Text>
                     <Image
                         source={src}
                         //source={{uri:path}}
                         style={styles.imageSize}
                     />
-                    <Text style={styles.productModel}>{model}</Text>
+            <TouchableOpacity
+                style={styles.button}
+                onPress={whatsapp}
+            >
+                <Text style={{color:'white',fontSize:21,alignSelf:'center'}}>Enquire Now</Text>
+            </TouchableOpacity>
                 </View>
     );
 }
@@ -22,7 +39,7 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: 'white',
         alignSelf: 'center',
-        height: 320,
+        height: 350,
         width: '95%',
         borderRadius: 15,
         marginVertical: 10,
@@ -32,7 +49,7 @@ const styles = StyleSheet.create({
     imageSize: {
         alignSelf: 'center',
         width: '98%',
-        height: 270
+        height: 250
     },
     productModel: {
         alignSelf: 'center',
@@ -41,10 +58,20 @@ const styles = StyleSheet.create({
         marginTop:5,
         fontWeight: 'bold',
         borderWidth: 1,
-        borderRadius: 10,
+        borderRadius: 7,
         borderColor: '#194B7E',
         fontSize: 17,
         color: '#194B7E',
+    },
+    button: {
+        backgroundColor: '#194B7E',
+        alignSelf:'center',
+        width:170,
+        height:35,
+        borderRadius:7,
+        marginBottom:0,
+        marginTop:10,
+        elevation:10
     }
 })
 
